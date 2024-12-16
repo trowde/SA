b63b39ecff85262b326d875d2a98d2e5479770f7
Ryu22 <is0807fs@ed.ritsumei.ac.jp>
2024-12-16 14:45:59 +0900

    update

--- group_game/game.py
+++ group_game/game.py
@@ -14,18 +14,15 @@ SCREEN_RIGHT_BOUNDARY=SCREEN_WIDTH
 SCREEN_UP_BOUNDARY=0
 SCREEN_DOWN_BOUNDARY=SCREEN_HEIGHT
 
-
-
 NUMBER_OF_FORMULA=4
 RED=(255,0,0)
-WHITE=(255,255,255)
-BLACK=(0,0,0)
 
 #initiate
 screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('group_game')
-background=[pygame.image.load('level1.jpg'),pygame.image.load('level2.jpg'),pygame.image.load('level3.jpg'),pygame.image.load('level4.jpg')]
+background=[pygame.image.load('level1.jpg'),pygame.image.load('level2.jpg'),pygame.image.load('level3.jpg')]
 music=['level1.mp3','level2.mp3']
+play_index=0
 pygame.mixer.music.load("level1.mp3")
 pygame.mixer.music.set_volume(1)
 pygame.mixer.music.play(-1)
@@ -39,52 +36,33 @@ font=pygame.font.Font(None,36)
 #pygame.mixer.music.load(music[play_index])
 #pygame.mixer.music.play(-1)
 
+class player_figure():
 
-
-
-
-
-
-
-
-
- 
-
-
-
-
-#player
-class Player(pygame.sprite.Sprite):
-    number=1
-    speed=3.5
-    width=50
-    height=50
-    collider_width=10
-    collider_height=10
-    number_upper_limit=20
-    def __init__(self,x,y):
-        super().__init__()
-        self.position_X=x       
-        self.position_Y=y
-        self.rect=pygame.Rect(self.position_X-Player.collider_width/2,self.position_Y,Player.collider_width,Player.collider_height)
-    def base_figure(self):
-        self.rect=pygame.Rect(self.position_X-Player.collider_width/2,self.position_Y,Player.collider_width,Player.collider_height)
-        text=f"{Player.number}"
-        number_render=font.render(text,True,(255,0,0))
-        screen.blit(number_render,(self.position_X-10,self.position_Y-20))
-        screen.blit(image,(self.position_X-Player.width/2,self.position_Y))
-    def derivative_figure(self):
-        screen.blit(image,(self.position_X-Player.width/2,self.position_Y))
     @staticmethod
-    def create_derivative_figure():
-        if Player.number<=Player.number_upper_limit:
-            while len(Other.player_figure)>Player.number:
-                Other.player_figure.pop()
-            while len(Other.player_figure)<Player.number:
-                Other.player_figure.append(Player(Other.player_figure[0].position_X+random.uniform(-25,25),Other.player_figure[0].position_Y+random.uniform(0,50)))
-        else:
-            while len(Other.player_figure)<Player.number:
-                Other.player_figure.append(Player(Other.player_figure[0].position_X+random.uniform(-25,25),Other.player_figure[0].position_Y+random.uniform(0,50)))
+    def create(x,y):
+        screen.blit(image, (x+25, y+25)) 
+
+#player.initiate
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.position_X=SCREEN_WIDTH/2       
+        self.position_Y=SCREEN_HEIGHT-200
+        #self.image=pygame.image.load('player.png')
+        self.width=50
+        self.height=50
+        self.speed=3.5
+        self.number=1
+        self.rect=pygame.Rect(self.position_X+25,self.position_Y-25,0,0)
+    def draw_player(self,x,y,num):
+        text=f"{self.number}"
+        number_render=font.render(text,True,(255,0,0))
+        screen.blit(number_render,(x+20,y-25))
+        self.rect=pygame.Rect(x,y,50,50)
+        #for i in range(self.number):
+        player_figure.create(x,y)
+        #pygame.draw.rect(screen,RED,(x+10,y+10,5,20))
+
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
@@ -96,6 +74,15 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_DOWN]:
             self.position_Y+=self.speed;
 
+        #if self.position_X+self.width<SCREEN_LEFT_BOUNDARY:
+         #   self.position_X=SCREEN_RIGHT_BOUNDARY
+        #if self.position_X > SCREEN_RIGHT_BOUNDARY:  
+         #   self.position_X=SCREEN_LEFT_BOUNDARY-self.width
+        #if self.position_X < SCREEN_LEFT_BOUNDARY: 
+          #  screen.blit(self.image, (self.position_X + SCREEN_WIDTH, self.position_Y))
+       # elif self.position_X + self.width > SCREEN_RIGHT_BOUNDARY:  
+           # screen.blit(self.image, (self.position_X - SCREEN_WIDTH, self.position_Y))
+
 class Formula(pygame.sprite.Sprite):
     current_index=0
     number=4
@@ -103,8 +90,7 @@ class Formula(pygame.sprite.Sprite):
     width=20
     height=20
     operator=['+','-','*','/']
-    height=50
-    line_thickness=2
+    height=100
     def __init__(self,x,y,a,b):
         super().__init__()
         self.position_X=x
@@ -112,156 +98,123 @@ class Formula(pygame.sprite.Sprite):
         self.value=a
         self.operator=b
         self.text=f"{self.operator}{self.value}"
-        self.rect=pygame.Rect(self.position_X,self.position_Y,SCREEN_WIDTH/Formula.number,Formula.height)
-    def formula_move(self):
-        pygame.draw.rect(screen,WHITE,pygame.Rect(self.position_X,self.position_Y,SCREEN_WIDTH/NUMBER_OF_FORMULA,Formula.height))
-        pygame.draw.rect(screen,BLACK,pygame.Rect(self.position_X,self.position_Y,SCREEN_WIDTH/NUMBER_OF_FORMULA,Formula.height),Formula.line_thickness)
-        formula_render=font.render(self.text,True,BLACK)
-        screen.blit(formula_render,(self.position_X+20,self.position_Y+25))
-        self.rect=pygame.Rect(self.position_X,self.position_Y,SCREEN_WIDTH/Formula.number,Formula.height)
-
+        self.rect=pygame.Rect(x,self.position_Y,SCREEN_WIDTH/NUMBER_OF_FORMULA,Formula.height)
     def formula_reset(self):
         self.operator=Formula.operator[random.randint(0,3)]
         self.value=random.randint(1,10)
         self.position_Y=-50
         self.text=f"{self.operator}{self.value}"
-
+    @staticmethod
+    def formula_move(x,y,text,formula):
+        pygame.draw.rect(screen,(255,0,0),pygame.Rect(x,y,SCREEN_WIDTH/NUMBER_OF_FORMULA,Formula.height),2)
+        formula_render=font.render(text,True,(255,0,0))
+        screen.blit(formula_render,(x+20,y+25))
+        formula.rect=pygame.Rect(x,y,125,50)
 
     
+
+
+
+
+
 class Other(pygame.sprite.Sprite):
     background_speed=2
     background_position=0
     norm=0 
-    play_index=0
-    player_figure=[]
-    formulas=[]
-    player_figure.append(Player(SCREEN_WIDTH/2,SCREEN_HEIGHT-200))
-
-
-    for i in range(4):
-        formulas.append(Formula(i*SCREEN_WIDTH/NUMBER_OF_FORMULA,-100,random.randint(1,10),Formula.operator[random.randint(0,3)]))
-
-
     @staticmethod
     def background_move(y,play_index):
-            screen.blit(background[3],(0,y))
-            screen.blit(background[3],(0,-SCREEN_HEIGHT+y))
+            screen.blit(background[play_index],(0,y))
+            screen.blit(background[play_index],(0,-SCREEN_HEIGHT+y))
     @staticmethod
     def background_alter(y,play_index):
          screen.blit(background[play_index-1],(0,-SCREEN_HEIGHT+y))
          screen.blit(background[play_index],(0,-2*SCREEN_HEIGHT+y))
-    @staticmethod
-    def collide_caculate():
-        for i in range(NUMBER_OF_FORMULA):
-            if Other.player_figure[0].rect.colliderect(Other.formulas[i].rect):
-                if Other.formulas[i].operator=='+':
-                    Player.number=int(Player.number+Other.formulas[i].value)
-                elif Other.formulas[i].operator=='-':
-                    Player.number=int(Player.number-Other.formulas[i].value)
-                elif Other.formulas[i].operator=='*':
-                    Player.number=int(Player.number*Other.formulas[i].value)
-                elif Other.formulas[i].operator=='/':
-                    Player.number=int(Player.number/Other.formulas[i].value)
-                Formula.current_index+=1
-                if Other.play_index<=2:
-                    if Formula.current_index>=5:
-                        Formula.current_index-=5
-                        Other.play_index+=1
-                for j in range(4):
-                    Other.formulas[j].formula_reset()
-                break
 
 
-clock = pygame.time.Clock()
-running=True
-while running:
-
-
-    Other.collide_caculate()
-    Player.create_derivative_figure()
-    Other.background_move(Other.background_position,Other.play_index)   
-    Other.background_position+=Other.background_speed
-    if Other.background_position>SCREEN_HEIGHT:
-        Other.background_position=0
-    
-    Other.player_figure[0].base_figure()
-    Other.player_figure[0].move()
-    if Player.number<=20:
-        for i in range(1,Player.number):
-            Other.player_figure[i].derivative_figure()
-            Other.player_figure[i].move()
-    else:
-        for i in range(1,Player.number_upper_limit):
-            Other.player_figure[i].derivative_figure()
-            Other.player_figure[i].move()
-
-    for i in range(NUMBER_OF_FORMULA):
-        Other.formulas[i].formula_move()
-        Other.formulas[i].position_Y+=Formula.speed
-
-
-    if Player.number<=0:
-        running=False
-
-
-    for event in pygame.event.get():
-        if event.type ==pygame.QUIT:
-            running=False
-
-   
-    clock.tick(60) 
+def show_game_over_screen():
+    screen.fill((0, 0, 0))  # BLACK_background
+    game_over_text = font.render("GAME OVER", True, (255, 0, 0))
+    restart_text = font.render("Press R to Restart or Q to Quit", True, (255, 255, 255))
+    screen.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 2 - 50))
+    screen.blit(restart_text, (SCREEN_WIDTH // 2 - restart_text.get_width() // 2, SCREEN_HEIGHT // 2 + 20))
     pygame.display.update()
 
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # close window
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:  # R restart
+                    main_game()  # main_game continue
+                    waiting = False
+                if event.key == pygame.K_q:  # Q close
+                    pygame.quit()
+                    exit()
 
-   
+
+def main_game():
+  player=Player()
+  other=Other()
+  formulas=[]
+  play_index = 0
+  Other.background_position = 0
+  for i in range(4):
+      formulas.append(Formula(i*SCREEN_WIDTH/NUMBER_OF_FORMULA,-50,random.randint(1,10),Formula.operator[random.randint(0,3)]))
+
+  clock = pygame.time.Clock()
+  running=True
+  while running:
+
+
+      for i in range(4):
+          if player.rect.colliderect(formulas[i].rect):
+              if formulas[i].operator=='+':
+                  player.number+=formulas[i].value
+              elif formulas[i].operator=='-':
+                  player.number-=formulas[i].value
+              elif formulas[i].operator=='*':
+                  player.number*=formulas[i].value
+              elif formulas[i].operator=='/':
+                  player.number/=formulas[i].value
+              Formula.current_index+=1
+              if play_index<=2:
+                  if Formula.current_index>=5:
+                      Formula.current_index-=5
+                      play_index+=1
+              for j in range(4):
+                  formulas[j].formula_reset()
+              break
+      if (Formula.current_index>0&play_index>0)|(play_index==0&Formula.current_index<5):
+          Other.background_move(Other.background_position,play_index)    
+      else: 
+          other.background_alter(Other.background_position,play_index)
+      Other.background_position+=Other.background_speed
+      if Other.background_position>SCREEN_HEIGHT:
+          Other.background_position=0
+
+      
+      for i in range(NUMBER_OF_FORMULA):
+          Formula.formula_move(formulas[i].position_X,formulas[i].position_Y,formulas[i].text,formulas[i])
+          formulas[i].position_Y+=Formula.speed
+      
+
+
+      #player_figure.create(250,500)
+      player.draw_player(player.position_X,player.position_Y,player.number)
+      player.move()
+      if player.number<=0:
+          running=False
+          show_game_over_screen()
+      for event in pygame.event.get():
+          if event.type ==pygame.QUIT:
+              running=False
+
     
+      clock.tick(60) 
+      pygame.display.update()
 
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      #  if self.position_X+self.width<SCREEN_LEFT_BOUNDARY:
-     #       self.position_X=SCREEN_RIGHT_BOUNDARY
-    #self.position_X > SCREEN_RIGHT_BOUNDARY:  
-    # self.position_X=SCREEN_LEFT_BOUNDARY-self.width
-   # self.position_X < SCREEN_LEFT_BOUNDARY: 
-  #  screen.blit(self.image, (self.position_X + SCREEN_WIDTH, self.position_Y))
- #   elif self.position_X + self.width > SCREEN_RIGHT_BOUNDARY:  
-#    screen.blit(self.image, (self.position_X - SCREEN_WIDTH, self.position_Y))
-
-
-#background move&alter
-    #if (Formula.current_index>0&play_index>0)|(play_index==0&Formula.current_index<5):
-
-    #else: 
-         #other.background_alter(Other.background_position,play_index)
-#formula move    
-
-    
-
-#player move
-
-   # for i in range(len(player_figure)):
-    ##    Player.draw_figure(player_figure[i].position_X,player_figure[i].position_Y)
-
-
+main_game()
+pygame.quit()
